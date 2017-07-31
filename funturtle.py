cce8bec35bb3388b437fb15cb951fcd824a7d0a7
naama19-meet <naama19@meet.mit.edu>
2017-07-31 15:38:10 +0300

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,20 @@
+import turtle
+turtle.shape('turtle')
+t = turtle.clone()
+t.shape("triangle")
+
+#square.goto(100,0
+#square.goto(100,100)
+#square.goto(0,100)
+#square.goto(0,0_
+
+
+t.goto(100,0)
+t.goto(50,100)
+t.goto(0,0)
+
+
+turtle.penup()
+turtle.goto(0,-100)
+turtle.stamp()
+turtle.goto(0,-200)
