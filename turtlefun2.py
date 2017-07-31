6ed5d40e7f3f45881326671f1afbc3db6c16c432
naama19-meet <naama19@meet.mit.edu>
2017-07-31 17:33:13 +0200

    Stopping here

--- turtlefun2.py
+++ turtlefun2.py
@@ -1,5 +1,35 @@
-UP =  0
+import turtle
+UP_ARROW = 'Up'
+LEFT_ARROW = 'Left'
+DOWN_ERROW = 'Down'
+RIGHT_ERROW = 'Righ'
+SPACEBAR = 'space'
+
+UP = 0
 LEFT = 1
 DOWN = 2
 RIGHT = 3
 
+direction = UP
+
+def up():
+    global direction
+    direction = UP
+    print('you pressed up')
+          
+def down():
+    global direction
+    direction = DOWN
+    print('you pressed down')
+
+def left():
+    global direction
+    direction = LEFT
+    print('you pressed left')
+    
+def right():
+    global direction
+    direction = RIGHT
+    print('you pressed right')
+
+#turtle.mainloop()
